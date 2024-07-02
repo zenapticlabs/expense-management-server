2f64a97c2dc199fe9da0ff18eb5afbcc1be4e06a
Sung-Hyun Kang <skang@SKANGMBP.local>
2024-07-02 15:10:46 -0500

    settings file

--- Template/settings.py
+++ Template/settings.py
@@ -14,9 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -222,7 +220,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FIXTURE_DIRS = [
     os.path.join(BASE_DIR, 'fixtures')
 ]
