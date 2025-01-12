a2c185564b5c47cdf576b16384533441936c5420
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2025-01-12 16:56:45 -0600

    Added settings

--- Template/settings.py
+++ Template/settings.py
@@ -206,7 +206,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
